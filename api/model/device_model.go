4380783b7db45109c011345a135e7611be2abfab
hiroki-fukumoto <fuku0915@gmail.com>
2022-07-01 21:52:42 +0900

    fix: column type

--- api/model/device_model.go
+++ api/model/device_model.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Device struct {
 	Base
-	UserID            string `gorm:"type:char(36);index"`
+	UserID            string `gorm:"type:size:36;index"`
 	DeviceToken       string `gorm:"size:64;not null;unique"`
 	Os                string `gorm:"size:64;not null"`
 	CurrentAppVersion string `gorm:"size:16;not null"`
