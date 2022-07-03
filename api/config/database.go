bf99188cac559e0777cf8a6b65d29b715efb6b39
hiroki-fukumoto <fuku0915@gmail.com>
2022-07-03 10:42:00 +0900

    fix: docker compose db connect

--- api/config/database.go
+++ api/config/database.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"log"
 	"os"
+	"time"
 
 	"github.com/hiroki-Fukumoto/matching-app-api/api/model"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
@@ -16,18 +18,38 @@ var (
 )
 
 func Connect() *gorm.DB {
+	// var db *gorm.DB
+	err = dbConnect()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	autoMigration()
+	return db
+}
+
+func dbConnect() error {
 	godotenv.Load(".env")
 	godotenv.Load()
 
 	dsn := os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("DB_HOST") + ":3306)/" + os.Getenv("DB_NAME") + "?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic(err)
+
+	var count int = 10
+	// var db *gorm.DB
+	// var err error
+	for count > 1 {
+		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+		if err != nil {
+			time.Sleep(time.Second * 2)
+			count--
+			log.Printf("retry... count:%v\n", count)
+			continue
+		}
+		break
 	}
 
-	autoMigration()
-
-	return db
+	return err
 }
 
 func autoMigration() {
